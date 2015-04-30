dd7715edcdd0142646d001113f33b5fb486d0a0f
kenz16 <kendysusantho16@gmail.com>
2015-04-30 17:19:15 +0700

    test metadata

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -1,4 +1,14 @@
 class Api::ArticlesController < ApplicationController
+	def csrf_meta_tags
+	    if protect_against_forgery?
+	      [
+	        tag('meta', :name => 'csrf-param', :content => request_forgery_protection_token),
+	        tag('meta', :name => 'csrf-token', :content => form_authenticity_token),
+	      ].join("\n").html_safe
+	    end
+  	end
+
+
 	def index
     	articles = Article.all
     	jsonObject = articles.to_json(:include => :comments)
